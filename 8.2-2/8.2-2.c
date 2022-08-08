67564501ff42db6f3669416d26fa37f4e30a5a43
Merrill99 <540088919@qq.com>
2022-08-08 23:11:49 +0800

    implement factorial in recursion

---
+++ 8.2-2/8.2-2.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//µÝ¹éÊµÏÖ½×³Ë
+int Fac(int n)
+{
+	if (n <= 1)
+		return 1;
+	else
+		return n * Fac(n - 1);
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d\n", Fac(n));
+	return 0;
+}
